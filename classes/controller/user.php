11d4a1f99d7dbd11445e76cd3f327145395e1536
Fred Wu <canglan@gmail.com>
2009-10-31 23:21:49 +1100

    Controller usage example.

--- classes/controller/user.php
+++ classes/controller/user.php
@@ -2,6 +2,31 @@
 
 class Controller_User extends Controller
 {
+	public $authlite;
+	
+	public function before()
+	{
+		// Authlite instance
+		$this->authlite = Authlite::instance();
+		
+		// login check
+		if ( ! $this->authlite->logged_in() && Request::instance()->action != 'login') {
+			
+			$this->request->redirect('user/login');
+			
+		} else {
+			
+			// assigns the user object
+			$this->user = $this->authlite->get_user();
+			
+			if ($this->authlite->logged_in() && Request::instance()->action == 'login') {
+				$this->request->redirect('');
+			}
+		}
+		
+		parent::before();
+	}
+	
 	public function action_login()
 	{
 		empty($_POST) or $this->authlite->login($_POST['username'], $_POST['password'], TRUE);
